5aa33340f4907651d99e12d16f250a7e4e5a92d9
JE-FUNES <infoventacba@gmail.com>
2024-02-24 11:38:45 -0300

    archivo vercel

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 const server = require('./src/appServer.js');
 const { conn } = require('./src/db.js');
+const PORT = process.env.PORT || 3001;
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // { force: true } si quiero que se regenren las tablas
+conn
+.sync({ force: false })
+.then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`); // { force: true } si quiero que se regenren las tablas
   });
-});
+})
+.catch((error) => console.error(error));
