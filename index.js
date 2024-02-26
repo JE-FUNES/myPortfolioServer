ba61f71e29e675c58a26a58c0249a39c87976d9d
JE-FUNES <infoventacba@gmail.com>
2024-02-26 10:33:56 -0300

    new

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 
-{/*const server = require('./src/appServer.js');
-const { conn } = require('./src/db.js');
-const PORT = process.env.PORT || 3001;
-
-
-// Syncing all the models at once.
-conn
-.sync({ force: false })
-.then(() => {
-  server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`); // { force: true } si quiero que se regenren las tablas
-  });
-})
-.catch((error) => console.error(error));*/}
-
 const server = require('./src/appServer.js');
-const exprees = require('express');
-const server = exprees();
+
 
 const port = process.env.PORT || 3001;
 
 server.listen(port);
 
-console.log('Server listening on port', port);
+console.log(`Server listening on port ${port}`);
